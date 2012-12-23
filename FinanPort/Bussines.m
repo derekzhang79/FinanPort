3de44c744d53b7ae57b1a0a1a267185df289a1f3
Vega <Mahmood1@Lions-Mac.local>
2012-12-23 03:17:19 -0600

    Business Logic con formulas, condiciones, razones y mejoras en el StoryBoard

--- FinanPort/Bussines.m
+++ FinanPort/Bussines.m
@@ -32,44 +32,31 @@
     //NSLog(@"[razones count] %d", [razones count]);
     
     for (int i=0; i < [razones count]; i++) {
-        float temp1 = 0;
-        NSString *semaforo = [[NSString alloc] init];
+        float     valor        = 0;
+        float     numerador    = 0;
+        float     denominador  = 0;
+        NSString *semaforo     = [[NSString alloc] init];
         
-        //NSLog(@"titulo %@",[[razones objectAtIndex:i] titulo]);
+        //NSLog(@"Business.getRazonesByMonth-titulo %@",[[razones objectAtIndex:i] titulo]);
         
-        NSNumber *temp2 = [[NSNumber alloc] init];
-        //temp2 = [self calculateNumerador:[[razones objectAtIndex:i] numeradorFormula] :cuentas];
-        temp2 = [self calculateValue:[[razones objectAtIndex:i] numeradorFormula] :cuentas];
-        [[razones objectAtIndex:i] setNumerador:temp2];
-        //NSLog(@"numerador %d",[temp2 intValue]);
+        numerador = [self calculateValue:[[razones objectAtIndex:i] numeradorFormula] :cuentas];
+        [[razones objectAtIndex:i] setNumerador:[[NSNumber alloc] initWithFloat:numerador]];
+        //NSLog(@"Business.getRazonesByMonth-numerador: %f",numerador);
         
-        temp1 = [temp2 intValue];
+        denominador = [self calculateValue:[[razones objectAtIndex:i] denominadorFormula] :cuentas];
+        [[razones objectAtIndex:i] setDenominador:[[NSNumber alloc] initWithFloat:denominador]];
+        //NSLog(@"Business.getRazonesByMonth-numerador: %f",denominador);
         
-        //temp2 = [self calculateDenominador:[[razones objectAtIndex:i] denominadorFormula] :cuentas];
-        temp2 = [self calculateValue:[[razones objectAtIndex:i] denominadorFormula] :cuentas];
-        [[razones objectAtIndex:i] setDenominador:temp2];
-        //NSLog(@"denominador %d",[temp2 intValue]);
-        
-        if ([temp2 intValue] != 0) {
-            temp1 = (temp1 / [temp2 intValue]);
+        if (denominador != 0) {
+            valor = numerador/denominador;
             
-            //NSLog(@"total %f",temp1);
-            
-            temp2 = [[NSNumber alloc] initWithFloat:temp1];
-            [[razones objectAtIndex:i] setValor:[NSString stringWithFormat:@"%.02f",temp1]];
-            
-            //Rojo: Menor a 1
-            //Ambar: Caso 1 (Mayor a 1 y Menor a 2), Caso 2 (Mayor a 2)
-            //Verde: Igual 2
-            if (temp1 < 1) {
-                semaforo = @"semaforo_r.png";
-            }else if (temp1 > 1 &&  temp1 < 2) {
-                semaforo = @"semaforo_a.png";
-            }else if (temp1 == 2){
-                semaforo = @"semaforo_v.png";
-            }
-            //NSLog(@"semaforo %@",semaforo);
+            //NSLog(@"Business.getRazonesByMonth-valor: %f",valor);
+
+            [[razones objectAtIndex:i] setValor:[NSString stringWithFormat:@"%.02f",valor]];
+                       
+            semaforo = [self defineSemaforo :[[razones objectAtIndex:i] condicionesSemaforoVerde] :[[razones objectAtIndex:i] condicionesSemaforoAmarillo] :[[razones objectAtIndex:i] condicionesSemaforoRojo] :valor];
             [[razones objectAtIndex:i] setSemaforo:semaforo];
+            //NSLog(@"Business.getRazonesByMonth-semaforo %@",semaforo);
         } else {
             [[razones objectAtIndex:i] setValor:@"No aplica"];
             [[razones objectAtIndex:i] setSemaforo:@"semaforo_r.png"];
@@ -78,36 +65,171 @@
     return razones;
 }
 
-- (NSNumber *)calculateValue: (NSString *)formula: (NSMutableArray *)cuentas {
-    int index  = 0;
-    int result = 0;
-    int j = 0;
-    NSNumber *temp;
-    NSArray  *indexes = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"+-"]];
+- (float)calculateValue: (NSString *)formula: (NSMutableArray *)cuentas {
+    int       index      = 0;
+    float     resultado  = 0;
+    NSArray  *indexes    = [self getIndexes:formula];
+    NSArray  *signs      = [self getSigns:formula];
     
-    NSArray  *signs   = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet decimalDigitCharacterSet]];
+    for (int i=0; i < [indexes count]; i++) {
+        index  = [[indexes objectAtIndex:i] intValue];
+        if (i == 0) {
+            resultado = [[cuentas objectAtIndex:index] floatValue];
+        } else {
+            if ([[signs objectAtIndex:(i-1)] isEqualToString:@"+"]) {
+                resultado = resultado + [[cuentas objectAtIndex:index] floatValue];
+            } else if ([[signs objectAtIndex:(i-1)] isEqualToString:@"-"]) {
+                resultado = resultado - [[cuentas objectAtIndex:index] floatValue];
+            }
+        }
+    }
+    //NSLog(@"Business.calculateValue-result: %f", resultado);
+    return resultado;
+}
+
+- (NSArray *)getIndexes :(NSString *)formula {
+    NSArray        *temp;
+    NSMutableArray *resultado = [[NSMutableArray alloc] init];
     
-    //NSLog(@"%@", indexes);
-    //NSLog(@"%@", signs);
+    temp = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"+- "]];
+    //NSLog(@"Business.getIndexes-raw: %@", temp);
+    for (int i = 0; i < [temp count]; i++) {
+        if ([[temp objectAtIndex:i] length] > 0) {
+            [resultado addObject:[temp objectAtIndex:i]];
+        }
+    }
+    //NSLog(@"Business.getIndexes-resultado: %@", resultado);
+    return [[NSArray alloc] initWithArray:resultado];
+}
+
+- (NSArray *)getSigns :(NSString *)formula {
+    NSArray        *temp;
+    NSMutableArray *resultado = [[NSMutableArray alloc] init];
     
-    index  = [[indexes objectAtIndex:j] intValue];
-    result = [[cuentas objectAtIndex:index] intValue];
-    for (int i=0; i < [signs count]; i++) {
-        if ([[signs objectAtIndex:i] isEqualToString:@"+"]) {
-            j++;
-            index  = [[indexes objectAtIndex:j] intValue];
-            result = result + [[cuentas objectAtIndex:index] intValue];
-        } else if ([[signs objectAtIndex:i] isEqualToString:@"-"]) {
-            j++;
-            index  = [[indexes objectAtIndex:(j)] intValue];
-            result = result - [[cuentas objectAtIndex:index] intValue];
+    temp = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"0123456789 "]];
+    //NSLog(@"Business.getIndexes-raw: %@", temp);
+    for (int i = 0; i < [temp count]; i++) {
+        if ([[temp objectAtIndex:i] length] != 0) {
+            [resultado addObject:[temp objectAtIndex:i]];
+        }
+    }
+    //NSLog(@"Business.getIndexes-resultado: %@", resultado);
+    return [[NSArray alloc] initWithArray:resultado];
+}
+
+- (NSString *)defineSemaforo :(NSString *)condicionesSemaforoVerde :(NSString *)condicionesSemaforoAmarillo :(NSString *)condicionesSemaforoRojo :(float)valor {
+    
+    if ([condicionesSemaforoVerde length] > 0) {
+        if ([self validateConditions :condicionesSemaforoVerde :valor]) {
+            //NSLog(@"Business.defineSemaforo-semaforo_v.png");
+            return @"semaforo_v.png";
         }
     }
     
-    //NSLog(@"result: %d", result);
-    temp = [[NSNumber alloc] initWithInt:result];
+    if ([condicionesSemaforoAmarillo length] > 0) {
+        if ([self validateConditions :condicionesSemaforoAmarillo :valor]) {
+            //NSLog(@"Business.defineSemaforo-semaforo_a.png");
+            return @"semaforo_a.png";
+        }
+    }
+
+    if ([condicionesSemaforoAmarillo length] > 0) {
+        if ([self validateConditions :condicionesSemaforoRojo :valor]) {
+            //NSLog(@"Business.defineSemaforo-semaforo_r.png");
+            return @"semaforo_r.png";
+        }
+    }
     
-    return temp;
+    return @"semaforo_r.png";
+}
+
+- (Boolean)validateConditions :(NSString *)condicion :(float)valor{
+    Boolean   resultado   = 0;
+    NSArray  *condiciones = [self getConditions:condicion];
+    NSArray  *signs       = [self getLogicSigns:condicion];
+    
+    for (int i=0; i < [condiciones count]; i++) {
+        if (i == 0) {
+            resultado = [self validateCondition:[condiciones objectAtIndex:i] :valor];
+        } else {
+            if ([[signs objectAtIndex:(i-1)] isEqualToString:@"&"]) {
+                resultado = resultado && [self validateCondition:[condiciones objectAtIndex:i] :valor];
+            } else if ([[signs objectAtIndex:(i-1)] isEqualToString:@"|"]) {
+                resultado = resultado || [self validateCondition:[condiciones objectAtIndex:i] :valor];
+            }
+        }
+    }
+    return resultado;;
+}
+
+- (NSArray *)getConditions :(NSString *)condicion {
+    NSArray        *temp;
+    NSMutableArray *resultado = [[NSMutableArray alloc] init];
+    
+    temp = [condicion componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"&| "]];
+    //NSLog(@"Business.getConditions-raw: %@", temp);
+    for (int i = 0; i < [temp count]; i++) {
+        if ([[temp objectAtIndex:i] length] != 0) {
+            [resultado addObject:[temp objectAtIndex:i]];
+        }
+    }
+    //NSLog(@"Business.getConditions-resultado: %@", resultado);
+    return [[NSArray alloc] initWithArray:resultado];
+}
+
+- (NSArray *)getLogicSigns :(NSString *)condicion {
+    NSArray        *temp;
+    NSMutableArray *resultado = [[NSMutableArray alloc] init];
+    
+    temp = [condicion componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>=.1234567890 "]];
+    //NSLog(@"Business.getLogicSigns-raw: %@", temp);
+    for (int i = 0; i < [temp count]; i++) {
+        if ([[temp objectAtIndex:i] length] != 0) {
+            [resultado addObject:[temp objectAtIndex:i]];
+        }
+    }
+    //NSLog(@"Business.getLogicSigns-resultado: %@", resultado);
+    return [[NSArray alloc] initWithArray:resultado];
+}
+
+- (Boolean)validateCondition :(NSString *)condicion :(float)valor1{
+    float     valor2    = 0;
+    NSString *temp      = [condicion stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>= "]];
+    
+    valor2 = [temp floatValue];
+    NSLog(@"Business.validateCondition-valor1: %f valor2: %f", valor1, valor2);
+    if ([condicion hasPrefix:@"<="]) {
+        if (valor1 <= valor2) {
+            //NSLog(@"Business.validateCondition-TRUE");
+            return TRUE;
+        }
+    }
+    if ([condicion hasPrefix:@">="]) {
+        if (valor1 >= valor2) {
+            //NSLog(@"Business.validateCondition-TRUE");
+            return TRUE;
+        }
+    }
+    if ([condicion hasPrefix:@"="]) {
+        if (valor1 == valor2) {
+            //NSLog(@"Business.validateCondition-TRUE");
+            return TRUE;
+        }
+    }
+    if ([condicion hasPrefix:@"<"]) {
+        if (valor1 < valor2) {
+            //NSLog(@"Business.validateCondition-TRUE");
+            return TRUE;
+        }
+    }
+    if ([condicion hasPrefix:@">"]) {
+        if (valor1 > valor2) {
+            //NSLog(@"Business.validateCondition-TRUE");
+            return TRUE;
+        }
+    }
+    //NSLog(@"Business.validateCondition-FALSE");
+    return FALSE;
 }
 
 @end
--- FinanPort/FinanPortDAO.m
+++ FinanPort/FinanPortDAO.m
@@ -15,6 +15,9 @@
 - (NSMutableArray *)getRazones {
     Razon          *razon;
     NSMutableArray *razones;
+    NSString       *formula;
+    NSArray        *formulas;
+    
     
     razones = [[NSMutableArray alloc] init];
     
@@ -22,10 +25,127 @@
     [razon setTitulo:@"Razón del Capital del Trabajo"];
     [razon setSignificado:@"Capacidad de pago a corto plazo de la empresa e índice de solvencia."];
     [razon setLectura:@"Cada $1.00 de obligaciones a corto plazo, está garantizado con $%@ de efectivo y otros bienes, que en el curso normal de operaciones se transforma en efectivo."];
-    [razon setNumeradorFormula:@"0+1+2+3-4+5+6+7+8+9+10"];
-    [razon setTituloDelNumerador:@"Efectivo y Otros Bienes"];
-    [razon setDenominadorFormula:@"24+25+26+27+28+29"];
-    [razon setTitulo:@"Obligaciones a Corto Plazo"];
+    
+    formula = @"(0+1+2+3-4+5+6+7+8+9+10)/(24+25+26+27+28+29)";
+    [razon setFormula:formula];
+    
+    formulas = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"FinanPort.getRazones: %@", formulas);
+    for (int i = 0; i < [formulas count]; i++) {
+        if (i == 0) {
+            [razon setNumeradorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelNumerador:@"Efectivo y Otros Bienes"];
+        } else if (i == 1) {
+            [razon setDenominadorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelDenomidor:@"Obligaciones a Corto Plazo"];
+        }
+    }
+    [razon setCondicionesSemaforoVerde:@"=2"];
+    [razon setCondicionesSemaforoAmarillo:@">1&<2|>2"];
+    [razon setCondicionesSemaforoRojo:@"<1"];
+
+    [razones addObject:razon];
+    
+    razon = [[Razon alloc] init];
+    [razon setTitulo:@"Razón del Margen de Seguridad"];
+    [razon setSignificado:@"Muestra la realidad de las inversiones tanto de los acreedores a corto plazo, como la de los acreedores a largo plazo y propietarios."];
+    [razon setLectura:@"Cada $1.00 de inversión en el activo circulante de los acreedores a corto plazo, los propietarios y acreedores a largo plazo invierten $%@."];
+    
+    formula = @"(0+1+2+3-4+5+6+7+8+9+10-25-26-27-28-29)/(24+25+26+27+28+29)";
+    [razon setFormula:formula];
+    
+    formulas = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"FinanPort.getRazones: %@", formulas);
+    for (int i = 0; i < [formulas count]; i++) {
+        if (i == 0) {
+            [razon setNumeradorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelNumerador:@"Capital del Trabajo"];
+        } else if (i == 1) {
+            [razon setDenominadorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelDenomidor:@"Pasivo Circulante"];
+        }
+    }
+    [razon setCondicionesSemaforoVerde:@">=2"];
+    [razon setCondicionesSemaforoAmarillo:@""];
+    [razon setCondicionesSemaforoRojo:@"<1"];
+    
+    [razones addObject:razon];
+    
+    [razones addObject:razon];
+    
+    razon = [[Razon alloc] init];
+    [razon setTitulo:@"Razón Severa o Prueba del Ácido"];
+    [razon setSignificado:@"La suficiencia o insuficiencia de la empresa, para cubrir los pasivos a corto plazo, es decir la solvencia inmediata de la empresa."];
+    [razon setLectura:@"Cada $1.00 de obligaciones a corto plazo, la empresa cuenta con $%@ centavos de activos."];
+    
+    formula = @"(0+1+2+3-4+5-6-7-8+9+10)/(24+25+26+27+28+29)";
+    [razon setFormula:formula];
+    
+    formulas = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"FinanPort.getRazones: %@", formulas);
+    for (int i = 0; i < [formulas count]; i++) {
+        if (i == 0) {
+            [razon setNumeradorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelNumerador:@"Solvencia Inmediata"];
+        } else if (i == 1) {
+            [razon setDenominadorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelDenomidor:@"Pasivo Circulante"];
+        }
+    }
+    [razon setCondicionesSemaforoVerde:@">=1&<=2"];
+    [razon setCondicionesSemaforoAmarillo:@">2"];
+    [razon setCondicionesSemaforoRojo:@"<1"];
+    
+    [razones addObject:razon];
+    
+    razon = [[Razon alloc] init];
+    [razon setTitulo:@"Razón de Protección al Pasivo Total"];
+    [razon setSignificado:@"La protección que ofrecen los propietarios a los acreedores, la capacidad de crédito de la empresa o ¿quién controla la empresa, acreedores o propietarios?"];
+    [razon setLectura:@"Los propietarios invierten $%@ en la empresa, por cada $1.00 de inversión de los acreedores."];
+    
+    formula = @"(33+58-22)/(24+25+26+27+28+29+30+31+32)";
+    [razon setFormula:formula];
+    
+    formulas = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"FinanPort.getRazones: %@", formulas);
+    for (int i = 0; i < [formulas count]; i++) {
+        if (i == 0) {
+            [razon setNumeradorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelNumerador:@"Capital Contable Tangible"];
+        } else if (i == 1) {
+            [razon setDenominadorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelDenomidor:@"Pasivo Total"];
+        }
+    }
+    [razon setCondicionesSemaforoVerde:@">=1"];
+    [razon setCondicionesSemaforoAmarillo:@""];
+    [razon setCondicionesSemaforoRojo:@"<1"];
+    
+    [razones addObject:razon];
+    
+    razon = [[Razon alloc] init];
+    [razon setTitulo:@"Razón de Protección al Pasivo Circulante"];
+    [razon setSignificado:@"Protección o garantía que ofrecen los propietarios a los acreedores a corto plazo."];
+    [razon setLectura:@"Los propietarios invierten $%@ por cada $1.00 de inversión de los acreedores al corto Plazo."];
+    
+    formula = @"(33+58-22)/(24+25+26+27+28+29)";
+    [razon setFormula:formula];
+    
+    formulas = [formula componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"/"]];
+    NSLog(@"FinanPort.getRazones: %@", formulas);
+    for (int i = 0; i < [formulas count]; i++) {
+        if (i == 0) {
+            [razon setNumeradorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelNumerador:@"Capital Contable Tangible"];
+        } else if (i == 1) {
+            [razon setDenominadorFormula:[[formulas objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"()"]]];
+            [razon setTituloDelDenomidor:@"Pasivo Circulante"];
+        }
+    }
+    [razon setCondicionesSemaforoVerde:@""];
+    [razon setCondicionesSemaforoAmarillo:@""];
+    [razon setCondicionesSemaforoRojo:@""];
+    
     [razones addObject:razon];
     
     return razones;
